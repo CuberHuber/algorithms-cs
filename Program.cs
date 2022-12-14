d3808ba8aac131855c7b25d233d0ffb71c34d842
Roman <komogvr@gmail.com>
2022-12-14 20:19:17 +0500

    add Collector.cs for merging one sorted Series from multiple series

--- Program.cs
+++ Program.cs
@@ -4,9 +4,27 @@ using algorithms_cs.Algorithm.Sort.External.Merge;
 using algorithms_cs.Tape;
 using algorithms_cs.Serial;
 
-Console.WriteLine(string.Format("fff {0}", "123"));
 
-var mws = new MultiwaySort(3, "Hello");
+var b1 = new BufferedTapeReader((string)"E:\\projects\\algorithms-cs\\resource\\1.test");
+var b2 = new BufferedTapeReader((string)"E:\\projects\\algorithms-cs\\resource\\2.test");
+var b3 = new BufferedTapeReader((string)"E:\\projects\\algorithms-cs\\resource\\3.test");
+
+var seriesList = new List<Series>();
+
+seriesList.Add(new Series(b1));
+seriesList.Add(new Series(b2));
+seriesList.Add(new Series(b3));
+
+var collector = new Collector(seriesList);
+
+SeriesReturn<double> value;
+do
+{
+    value = collector.Next();
+    Console.Write( value.GetType() == SeriesReturnType.Correct ? value.GetValue() + " " : "");
+} while (value.GetType() == SeriesReturnType.Correct);
+
+
 
 
 
--- src/Algorithm/Sort/External/Merge/Collector.cs
+++ src/Algorithm/Sort/External/Merge/Collector.cs
@@ -1,31 +1,60 @@
+using System.Diagnostics;
 using algorithms_cs.Serial;
 
 namespace algorithms_cs.Algorithm.Sort.External.Merge;
 
+internal struct MinReturn
+{
+    public SeriesReturn<double> element;
+    public int index;
+}
 
 public class Collector
 {
-    private readonly int _countSeries;
-    private readonly Series[] _seriesCollection;
+    private readonly List<Series> _seriesCollection;
+    private List<SeriesReturn<double>> _bufferSeriesReturns;
 
-    public Collector(int countSeries, Series[] SeriesCollection)
+    public Collector(List<Series> seriesCollection)
     {
-        _countSeries = countSeries;
-        _seriesCollection = SeriesCollection;
-        CheckOutInitData();
-    }
-    
-    // Сделалть Тип проверки ( меньше, больше )
-    private void CheckOutInitData()
-    {
-        if (_seriesCollection.Length != _countSeries)
+        _seriesCollection = seriesCollection;
+        _bufferSeriesReturns = new List<SeriesReturn<double>>();
+        foreach (var seriesReturn in _seriesCollection)
         {
-            throw new Exception();
+            _bufferSeriesReturns.Add(seriesReturn.Next());
         }
+    }
+
+    private MinReturn Min()
+    {
+        // This method find Correct minimal value from multiple Series
+        
+        var value = double.MaxValue;
+        var minReturn = new MinReturn();
+        minReturn.index = 0;
+
+        for (int i = 0; i < _bufferSeriesReturns.Count; i++)
+        {
+            if (_bufferSeriesReturns[i].GetType() == SeriesReturnType.Correct && value > _bufferSeriesReturns[i].GetValue())
+            {
+                value = _bufferSeriesReturns[i].GetValue();
+                minReturn.element = _bufferSeriesReturns[i];
+                minReturn.index = i;
+            }
+        }
+
+        return minReturn;
     }
 
     public SeriesReturn<double> Next()
     {
+        // Returns minimal Correct value from multiple Series, else returns SeriesEnd
+
+        var seriesEnded = _bufferSeriesReturns.All(x => x.GetType() != SeriesReturnType.Correct);
+        if (seriesEnded) return new SeriesReturn<double>(SeriesReturnType.SeriesEnded);
+
+        var minElement = Min();
         
+        _bufferSeriesReturns[minElement.index] = _seriesCollection[minElement.index].Next();
+        return minElement.element;
     }
 }
