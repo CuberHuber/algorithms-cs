cb9324fe4578b3120ccd51af89769fcb14d98227
Roman <komogvr@gmail.com>
2022-12-14 21:25:43 +0500

    add TapeWriter.cs for write value in tape (file)

--- src/Tape/TapeWriter.cs
+++ src/Tape/TapeWriter.cs
@@ -1,6 +1,24 @@
 ﻿namespace algorithms_cs.Tape;
 
-public class TapeWriter
-{
+public class TapeWriter<T> : Tape
+{   
+    private readonly StreamWriter _file;
     
+    protected TapeWriter(string filepath) : base(filepath)
+    {
+        try
+        {
+            _file = new StreamWriter(Filepath);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public void Write(T value)
+    {
+        _file.Write(value.ToString() + " ");
+    }
 }
