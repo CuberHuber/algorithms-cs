8bcadf5f8d6c8a9d9e0d09ace5ad8181bbe88138
Roman <komogvr@gmail.com>
2022-12-18 16:44:53 +0500

    Добавил перечисляемые серии.

---
+++ src/Serial/SeriesEnumerable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using algorithms_cs.Tape;
+
+namespace algorithms_cs.Serial;
+
+/// <summary>
+/// SeriesEnumerable represents Series over the tape like a collection for using foreach
+/// Series representing a sequence of increasing numbers over tape.
+/// </summary>
+public class SeriesEnumerable: IEnumerable
+{
+    private readonly BufferedTapeReader _tape;
+    
+    public SeriesEnumerable(BufferedTapeReader tape)
+    {
+        _tape = tape;
+    }
+    
+    public IEnumerator GetEnumerator()
+    {
+        return new SeriesEnumerator(_tape);
+    }
+}
---
+++ src/Serial/SeriesEnumerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using algorithms_cs.Tape;
+
+namespace algorithms_cs.Serial;
+
+public class SeriesEnumerator: IEnumerator
+{
+    private readonly BufferedTapeReader _tape;
+    private double _previous;
+    
+    public SeriesEnumerator(BufferedTapeReader tape)
+    {
+        _tape = tape;
+        _previous = double.MinValue;
+    }
+    
+    /// <summary>
+    /// Checks a correct of next number
+    /// </summary>
+    /// <returns>boolean indicating whether a next number is available and more than the previous number</returns>
+    public bool MoveNext()
+    {
+        var value = _tape.Peek();
+        if (value.GetType() == TapeReturnType.Correct)
+        {
+            if (_previous <= value.GetValue())
+            {
+                _previous = value.GetValue();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Reset()
+    {
+    }
+
+    public object Current => _tape.Next().GetValue();
+}
